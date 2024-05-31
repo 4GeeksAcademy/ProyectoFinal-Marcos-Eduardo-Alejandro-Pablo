8bcb9a160897686c8d681c881b5795a159c44739
Eduardo Figueroa <156800901+EduardoFigueroa92@users.noreply.github.com>
2024-05-31 18:21:19 +0000

    cambios

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -14,7 +14,13 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			peliculasFavoritas: [],
+			peliculasPendientes: [],
+			peliculasVistas: [],
+			Blacklist: [],
+
+
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -47,93 +53,58 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
-			}
+			},
+			setFavoritas: (item) => {
+				const store = getStore()
+				if (!store.peliculasFavoritas.includes(item)) {
+					setStore({ peliculasFavoritas: [...store.peliculasFavoritas, item] })
+				}
+			},
+			removeFromFavoritas: (index) => {
+				const store = getStore();
+				const updatedFavoritos = [...store.peliculasFavoritas];
+				updatedFavoritos.splice(index, 1);
+				setStore({ ...store, peliculasFavoritas: updatedFavoritos });
+			},
+			setPendientes: (item) => {
+				const store = getStore()
+				if (!store.peliculasPendientes.includes(item)) {
+					setStore({ peliculasPendientes: [...store.peliculasPendientes, item] })
+				}
+			},
+			removeFromPendientes: (index) => {
+				const store = getStore();
+				const updatedPendientes = [...store.peliculasPendientes];
+				updatedPendientes.splice(index, 1);
+				setStore({ ...store, peliculasPendientes: updatedPendientes });
+			},
+			setVistas: (item) => {
+				const store = getStore()
+				if (!store.peliculasVistas.includes(item)) {
+					setStore({ peliculasVistas: [...store.peliculasVistas, item] })
+				}
+			},
+			removeFromVistas: (index) => {
+				const store = getStore();
+				const updatedVistas = [...store.peliculasVistas];
+				updatedVistas.splice(index, 1);
+				setStore({ ...store, peliculasVistas: updatedVistas });
+			},
+			setBlacklist: (item) => {
+				const store = getStore()
+				if (!store.listaNegra.includes(item)) {
+					setStore({ listaNegra: [...store.listaNegra, item] })
+				}
+			},
+			removeFromBlacklist: (index) => {
+				const store = getStore();
+				const updatedNegra = [...store.listaNegra];
+				updatedNegra.splice(index, 1);
+				setStore({ ...store, listaNegra: updatedNegra });
+			},
+
 		}
 	};
 };
 
 export default getState;
-
-// const getState = ({ getStore, getActions, setStore }) => {
-// 	return {
-// 		store: {
-// 			message: null,
-// 			demo: [
-// 				{
-// 					title: "FIRST",
-// 					background: "white",
-// 					initial: "white"
-// 				},
-// 				{
-// 					title: "SECOND",
-// 					background: "white",
-// 					initial: "white"
-// 				}
-// 			],
-// 			peliculasFavoritas: [],
-// 			peliculasPendientes: [],
-// 			peliculasVistas: [],
-// 			listaNegra: [],
-
-
-// 			//añadir arrays de demás listas
-// 		},
-// 		actions: {
-
-// 			setFavoritas: (item) => {
-// 				const store = getStore()
-// 				if (!store.peliculasFavoritas.includes(item)) {
-// 					setStore({ peliculasFavoritas: [...store.peliculasFavoritas, item] })
-// 				}
-// 			},
-// 			removeFromFavoritas: (index) => {
-// 				const store = getStore();
-// 				const updatedFavoritos = [...store.peliculasFavoritas];
-// 				updatedFavoritos.splice(index, 1);
-// 				setStore({ ...store, peliculasFavoritas: updatedFavoritos });
-// 			},
-
-// 			setPendientes: (item) => {
-// 				const store = getStore()
-// 				if (!store.peliculasPendientes.includes(item)) {
-// 					setStore({ peliculasPendientes: [...store.peliculasPendientes, item] })
-// 				}
-// 			},
-// 			removeFromPendientes: (index) => {
-// 				const store = getStore();
-// 				const updatedPendientes = [...store.peliculasPendientes];
-// 				updatedPendientes.splice(index, 1);
-// 				setStore({ ...store, peliculasPendientes: updatedPendientes });
-// 			},
-
-// 			setVistas: (item) => {
-// 				const store = getStore()
-// 				if (!store.peliculasVistas.includes(item)) {
-// 					setStore({ peliculasVistas: [...store.peliculasVistas, item] })
-// 				}
-// 			},
-// 			removeFromVistas: (index) => {
-// 				const store = getStore();
-// 				const updatedVistas = [...store.peliculasVistas];
-// 				updatedVistas.splice(index, 1);
-// 				setStore({ ...store, peliculasVistas: updatedVistas });
-// 			},
-
-// 			setNegra: (item) => {
-// 				const store = getStore()
-// 				if (!store.listaNegra.includes(item)) {
-// 					setStore({ listaNegra: [...store.listaNegra, item] })
-// 				}
-// 			},
-// 			removeFromNegra: (index) => {
-// 				const store = getStore();
-// 				const updatedNegra = [...store.listaNegra];
-// 				updatedNegra.splice(index, 1);
-// 				setStore({ ...store, listaNegra: updatedNegra });
-// 			},
-// 		}
-
-// 	};
-// };
-
-// export default getState;
